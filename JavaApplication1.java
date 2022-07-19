cd5a3848d1a8f753462c97e6d874e7922d0be7ec
Karina Duran Munhos <63967857+KarinaDuran@users.noreply.github.com>
2022-07-19 11:33:31 -0300

    Add files via upload
    Commit inicial

---
+++ JavaApplication1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Museu;
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+/**
+ *
+ * @author karinamunhos
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   SwingUtilities.invokeLater(new Runnable()
+ {
+ 
+    // roda o a janela
+    @Override
+    public void run()
+    {
+
+   PaginaPrincipal p =  new PaginaPrincipal();
+   //p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    p.setVisible(true);
+     
+
+    }
+   });
+    }
+}
